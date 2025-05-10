641bafe45a95000d4054608f51f165feaf2f2a3c
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-05-09 22:00:03 -0300

    feat: update Card component to accept props for dynamic content and enhance styles

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -2,13 +2,20 @@ import { ImageSlider } from "..";
 
 import styles from "./styles/card.module.scss";
 
-export const Card = () => {
+type CardProps = {
+  id: string;
+  brand: string;
+  title: string;
+  images: string[];
+}
+
+export const Card = ({ id, brand, title, images }: CardProps) => {
   return (
     <div className={styles.card}>
-      <ImageSlider images={["/deal_dock_logo.png", "/deal_dock_logo.png", "/deal_dock_logo.png"]} />
+      <ImageSlider images={images} />
       <div className={styles.card__details}>
-        <span className={styles.card__brand}>MOTOROLA</span>
-        <h3 className={styles.card__title}>Smartphone Moto G75 5g 256gb Vegan Leather Cor Cinza Motorola</h3>
+        <span className={styles.card__brand}>{brand}</span>
+        <h3 className={styles.card__title}>{title}</h3>
       </div>
     </div>
   )
