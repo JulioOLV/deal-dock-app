792a98a635d6e2e00afdca7f71c43f0a9d228d3f
JulioOLV <julioccolv@gmail.com>
2025-05-10 20:30:09 -0300

    feat: update Home component to fetch and display product list based on search parameters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
+import { cache } from "react";
+
 import { Navbar, ProductList } from "./components";
 
 import styles from "./styles/page.module.scss";
+import { ProductAPIResponse } from "@/domain/models";
+import { makeListProductUseCase } from "@/main/factories/usecases/list-product-factory";
 
 interface SearchPageProps {
-  searchParams: { product?: string }
+  searchParams: { product?: string };
 }
 
 export default async function Home({ searchParams }: SearchPageProps) {
   const { product: searchByProduct } = await searchParams;
+
+  const listProduct = makeListProductUseCase(searchByProduct || "smartphone");
+
+  const getProducts = cache(
+    async (): Promise<ProductAPIResponse> => {
+      try {
+        const httpResponse = await listProduct.getProductList();
   
+        return httpResponse;
+      } catch (err: any) {
+        throw err;
+      }
+    }
+  );
+
+  const products = await getProducts();
+
   return (
     <>
       <Navbar />
       <main className={styles.container}>
-        <ProductList product={searchByProduct} />
+        <ProductList products={products} />
       </main>
       <footer className={styles.footer}>
         <p>Footer</p>
