7cd43c0fbcefbdac5feeb116379cef0a319ce7ec
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-05-09 22:01:09 -0300

    feat: add ProductList component to fetch and display products using Card component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./navbar/navbar";
 export * from "./image_slider/image_slider";
 export * from "./card/card";
+export * from "./product_list/product_list";
---
+++ src/app/components/product_list/product_list.tsx
@@ -0,0 +1,103 @@
+import { Card } from "../card/card";
+
+import styles from "./styles/product_list.module.scss";
+
+type ProductAttribute = {
+  id: string;
+  name: string;
+  value_id?: string;
+  value_name: string;
+};
+
+type ProductPicture = {
+  id: string;
+  url: string;
+};
+
+type ProductSettings = {
+  listing_strategy: string;
+  exclusive: boolean;
+};
+
+type ProductResultItem = {
+  id: string;
+  date_created: string;
+  catalog_product_id: string;
+  pdp_types: string[];
+  status: string;
+  domain_id: string;
+  settings: ProductSettings;
+  name: string;
+  main_features: string[];
+  attributes: ProductAttribute[];
+  pictures: ProductPicture[];
+  parent_id?: string;
+  children_ids: string[];
+  quality_type: string;
+  priority: string;
+  type: string;
+  site_id: string;
+  variations: string[];
+  keywords: string;
+  description: string;
+};
+
+type Paging = {
+  total: number;
+  limit: number;
+  offset: number;
+};
+
+type UsedAttribute = {
+  id: string;
+  name: string;
+  value_id?: string;
+  value_name: string;
+};
+
+export type ProductAPIResponse = {
+  keywords: string;
+  domain_id: string;
+  paging: Paging;
+  results: ProductResultItem[];
+  used_attributes: UsedAttribute[];
+  query_type: string;
+};
+
+const getProducts = async (): Promise<ProductAPIResponse> => {
+  const res = await fetch("https://api.mercadolibre.com/products/search?site_id=MLB&status=active&q=smartphone&domain_id=MLB-CELLPHONES&limit=12&offset=0", {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": `Bearer APP_USR-4177817543971930-050917-949f719e70533a1e113584117be18b82-171802850`,
+    },
+    cache: "force-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export const ProductList = async () => {
+  const products = await getProducts();
+
+  return (
+    <div className={styles.product_list}>
+      {
+        products.results.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            images={product.pictures.map((picture) => picture.url)}
+            brand={product.attributes.find((attr) => attr.id === "BRAND")?.value_name || "Desconhecido"}
+          />
+        ))
+      }
+    </div>
+  );
+}
