60193bf345af8618e57568654f042332a64628f3
JulioOLV <julioccolv@gmail.com>
2025-05-10 18:44:54 -0300

    feat: add error and loading components with styles for improved user experience

---
+++ src/app/error.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect } from "react";
+import { Navbar } from "./components";
+
+import styles from "./styles/error.module.scss";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        <div className={styles.home__error__container}>
+          <h2>Algo deu errado!</h2>
+          <button
+            className={styles.home__error__button}
+            onClick={
+              () => reset()
+            }
+          >
+            Tentar novamente
+          </button>
+        </div>
+      </main>
+    </>
+  );
+}
