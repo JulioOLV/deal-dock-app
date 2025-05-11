5778d5332bd416bff4ebe465a3dcaf52bee0ee4c
JulioOLV <julioccolv@gmail.com>
2025-05-11 17:21:29 -0300

    feat: integrate refresh token functionality into product list fetching logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./styles/page.module.scss";
 import { ProductAPIResponse } from "@/domain/models";
 import { makeListProductUseCase } from "@/main/factories/usecases/list-product-factory";
 import { Navbar, Pagination, ProductList } from "@/ui/components";
+import { makeRefreshTokenUseCase } from "@/main/factories/usecases/refresh-token-factory";
 
 interface SearchPageProps {
   searchParams: { product?: string; offset?: number };
@@ -12,11 +13,15 @@ interface SearchPageProps {
 export default async function Home({ searchParams }: SearchPageProps) {
   const { product: searchByProduct, offset } = await searchParams;
 
+  // Use expires_in to check if the token is expired
+  const refreshTokenUseCase = makeRefreshTokenUseCase();
+  const token = await refreshTokenUseCase.getToken();
+
   const listProductUseCase = makeListProductUseCase(
     searchByProduct || "smartphone",
-    offset || 0
+    offset || 0,
+    token.access_token
   );
-
   const getProducts = cache(async (): Promise<ProductAPIResponse> => {
     try {
       const httpResponse = await listProductUseCase.getProductList();
